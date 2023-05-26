a471fd16e3dc4bc95aa648323ad68c399812608b
Kyle Davis <kdavisj@outlook.com>
2023-05-26 00:55:59 -0400

    complete

---
+++ script.js
@@ -0,0 +1,21 @@
+const jokeElm = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeElm.innerHTML = data.joke
+}
